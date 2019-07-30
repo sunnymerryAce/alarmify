143017ea895779788b8ad874b97e087d5185f65e
sunnymerryAce <y.hirano.0104@gmail.com>
2019-07-30 18:31:28 +0900

    [fix] generalization

--- src/helper/CONFIG.js
+++ src/helper/CONFIG.js
@@ -2,15 +2,11 @@
  * Constant Class
  */
 const CONFIG = {
-  // Events
-  EVENT_LOAD_COMPLETE: 'loadComplete',
-  EVENT_OPEN_MODAL: 'openModal',
-  EVENT_FILTER_STORIES: 'filterStories',
-  TEST: {
-    CLASS_NAME: 'class',
-  },
-  // URI
-  REDIRECT_URI: 'https://alarmify-5f826.firebaseapp.com/',
+  // INSERT YOUR CONFIGURATION INFORMATION
+  // Spotiy Client ID
+  SPOTIFY_CLIENT_ID: '',
+  // Your Application URL
+  REDIRECT_URI: '',
 };
 
 export default CONFIG;
