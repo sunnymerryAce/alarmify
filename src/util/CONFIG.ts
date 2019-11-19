7a4a11a9e9003f8fbed70e86ed389a746d53df7f
sunnymerryAce <y.hirano.0104@gmail.com>
2019-11-19 12:20:19 +0900

    [add] Prettier config

---
+++ src/util/CONFIG.ts
@@ -0,0 +1,17 @@
+/**
+ * Configuration Data and Constants
+ */
+const CONFIG = {
+  // Error Message
+  ERROR: {
+    400: 'BAD_REQUEST',
+    401: 'UNAUTHORIZED',
+    403: 'FORBIDDEN',
+    404: 'NOT_FOUND',
+    500: 'INTERNAL_SERVER_ERROR',
+    502: 'BAD_GATEWAY',
+    default: 'UNHANDLED_ERROR',
+  },
+};
+
+export default CONFIG;
