a2df9ab6aee2c76a041a2dcc2922f465b1d3b3de
sunnymerryAce <y.hirano.0104@gmail.com>
2020-04-27 20:42:49 +0900

    [update] CI task firebase

--- scripts/setConfig.ts
+++ scripts/setConfig.ts
@@ -2,6 +2,9 @@ import { execSync } from 'child_process';
 import { readFileSync } from 'fs';
 import * as path from 'path';
 
+// set project
+execSync(`firebase use process.env.project_id`, { maxBuffer: 1024 * 1024, stdio: 'inherit' });
+
 const env = readFileSync(path.resolve(__dirname, '../.env'), { encoding: 'utf-8' });
 const lines = env.toString().split(/\n/g);
 lines.forEach((line) => {
