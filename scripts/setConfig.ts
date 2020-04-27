a7217c84fd1ed9b80fec47792167a5f5f2084886
sunnymerryAce <y.hirano.0104@gmail.com>
2020-04-27 20:47:03 +0900

    no message

--- scripts/setConfig.ts
+++ scripts/setConfig.ts
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs';
 import * as path from 'path';
 
 // set project
-execSync(`firebase use process.env.project_id`, { maxBuffer: 1024 * 1024, stdio: 'inherit' });
+execSync(`firebase use alarmify-5f826`, { maxBuffer: 1024 * 1024, stdio: 'inherit' });
 
 const env = readFileSync(path.resolve(__dirname, '../.env'), { encoding: 'utf-8' });
 const lines = env.toString().split(/\n/g);
